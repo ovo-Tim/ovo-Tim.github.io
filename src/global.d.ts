951042aea6e8c6ca673b711b6fa916ca80e36ab1
ovo-Tim <ovo-tim@qq.com>
2026-07-28 09:06:13 +0800

    feat: use material/web

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,20 @@ interface Window {
     opts?: { headers?: Record<string, string>; body?: any; method?: string }
   ): Promise<any>;
 }
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "md-elevated-card": any;
+    "md-outlined-card": any;
+    "md-assist-chip": any;
+    "md-icon-button": any;
+    "md-outlined-text-field": any;
+    "md-outlined-select": any;
+    "md-select-option": any;
+    "md-filled-button": any;
+    "md-text-button": any;
+    "md-outlined-button": any;
+    "md-divider": any;
+  }
+}
+
